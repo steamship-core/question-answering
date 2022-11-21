9fc0ce09cbeb3bb90cdc4c7796e75b79c57f410a
Ted Benson <ted@steamship.com>
2022-11-20 22:37:45 -0500

    wip

--- src/api.py
+++ src/api.py
@@ -1,70 +1,58 @@
-from typing import Dict
-from steamship import Steamship
-from steamship.app import App, Response, Error, post, create_handler
+"""Question Answering Package."""
 
-class QuestionAnswer(App):
-  def __init__(self, client: Steamship):
-    # In production, the lambda handler will provide a Steamship client:
-    # - Authenticated to the appropriate user
-    # - Bound to the appropriate space
-    self.client = client
-
-    # Create an embedding index using (for now!) our
-    # mock embedder.
-    #
-    # Note that the *scope* of this index is limited to the space
-    # this app is executing within. Each new instance of the app
-    # will resultingly have a fresh index.
-    self.index = self.client.create_index(
-      handle="qa-index",
-      plugin="test-embedder-v1"     
-    ).data
-  
-  @post('learn')
-  def learn(self, fact: str = None) -> Response:
-    """Learns a new fact."""
-    if fact is None:
-      return Error(message="Empty fact provided to learn.")
-   
-    if self.index is None:
-      return Error(message="Unable to initialize QA index.")
-
-    res = self.index.insert(fact, reindex=True)
-
-    if res.error:
-      # Steamship error messages can be passed straight
-      # back to the user
-      return Error(
-        message = res.error.message,
-        suggestion = res.error.suggestion,
-        code = res.error.code
-      )
-    
-    return Response(json=res.data)
-
-  @post('query')
-  def query(self, query: str = None, k: int = 1) -> Response:
-    """Learns a new fact."""
-    if query is None:
-      return Error(message="Empty query provided.")
-    
-    if self.index is None:
-      return Error(message="Unable to initialize QA index.")
-
-    res = self.index.search(query=query, k=k)
-
-    if res.error:
-      # Steamship error messages can be passed straight
-      # back to the user
-      return Error(
-        message = res.error.message,
-        suggestion = res.error.suggestion,
-        code = res.error.code
-      )
-    return Response(json=res.data)
+from steamship import EmbeddingIndex, PluginInstance, Steamship, SteamshipError
+from steamship.data.embeddings import IndexInsertResponse, QueryResults
+from steamship.invocable import PackageService, create_handler, post
 
 
-handler = create_handler(QuestionAnswer)
+class QuestionAnsweringPackage(PackageService):
+    """Simple question answering class that works with different embedding models."""
+
+    embedder: PluginInstance
+    index: EmbeddingIndex
+
+    def __init__(self, client: Steamship, **kwargs):
+        super().__init__(client, **kwargs)
+
+        # First we create an embedder. For this example package we'll use
+        # OpenAI's Davinci 001
+        self.embedder = self.client.use_plugin(
+            "openai-embedder",
+            config={"model": "text-similarity-davinci-001", "dimensionality": 12288},
+        )
+
+        self.index = EmbeddingIndex.create(
+            client=self.client,
+            handle="my-qa-index",
+            plugin_instance=self.embedder.handle,
+            fetch_if_exists=True,
+        )
+
+    @post("learn")
+    def learn(self, fact: str = None) -> IndexInsertResponse:
+        """Learns a new fact."""
+        if fact is None:
+            raise SteamshipError(message="Empty fact provided to learn.")
+
+        # Reindex is usually good to call right away -- this will make sure that the embedding
+        # gets created.
+        res = self.index.insert(fact, reindex=True)
+
+        # This is also good to do as it will help your index scale. This creates an AKNN
+        # structure on disk, as opposed to the KNN structure that would have otherwise been used.
+        self.index.create_snapshot()
+
+        return res
+
+    @post("query")
+    def query(self, query: str = None, k: int = 1) -> QueryResults:
+        """Learns a new fact."""
+        if query is None:
+            raise SteamshipError(message="Empty query provided.")
+
+        res = self.index.search(query=query, k=k)
+        res.wait()
+        return res.output
 
 
-
+handler = create_handler(QuestionAnsweringPackage)
